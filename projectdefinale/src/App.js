fd6e0a270912433d07e660799c0099b220cff7fc
Pal? <99118944+NauvalAqiilah@users.noreply.github.com>
2024-01-25 08:55:53 +0700

    Add files via upload

---
+++ projectdefinale/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar.js";
+import Content from "./components/Content.js";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Content />
+    </>
+  );
+};
+export default App;
